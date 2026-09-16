0fa03616e598e7fa587e55a4ae0439d948faac6d
Lays-Dev <penn2ink@gmail.com>
2026-09-15 20:22:50 -0400

    save point

--- Noxar Studio Game/Assets/Scripts/Gathering Minigame/GatheringUI.cs
+++ Noxar Studio Game/Assets/Scripts/Gathering Minigame/GatheringUI.cs
@@ -1,11 +1,39 @@
 using UnityEngine;
+using System.Collections;
+using TMPro;
 
 public class GatheringUI : MonoBehaviour
 {
+    // This panel will close later when a button is pressed.
     [SerializeField] private GameObject instructionsPanel;
 
+    [SerializeField] private GameObject countdownText;
+
+    // Function that closes the panel - used for a button.
     public void CloseInstructions()
     {
         instructionsPanel.SetActive(false);
+    }
+
+    // IEnumerator because this method will run for multiple frames
+    private IEnumerator Countdown()
+    {
+        countdownText.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+
+        countdownText.GetComponent<TMP_Text>().text = "2";
+
+        yield return new WaitForSeconds(1f);
+
+        countdownText.GetComponent<TMP_Text>().text = "1";
+
+        yield return new WaitForSeconds(1f);
+
+        countdownText.GetComponent<TMP_Text>().text = "GO!";
+
+        yield return new WaitForSeconds(1f);
+
+        countdownText.SetActive(false);
     }
 }
