8b7662d38ea74504815ba4ee7e65efa38c5a3988
Lays-Dev <penn2ink@gmail.com>
2026-09-18 17:01:45 -0400

    Art Prototype function idea1

---
+++ Noxar Studio Game/Assets/Scripts/Art Selection.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ArtSelection : MonoBehaviour
+{
+
+    [SerializeField] private GameObject playerArt;
+    [SerializeField] private GameObject art2;
+    public Button myButton;
+    public Button button2;
+
+    void Start()
+    {
+        playerArt.SetActive(false);
+        art2.SetActive(false);
+        button2.onClick.AddListener(Button2);
+         myButton.onClick.AddListener(ButtonClick);
+    }
+
+ 
+    void ButtonClick()
+    {
+        playerArt.SetActive(true);
+        art2.SetActive(false);
+    }
+
+    void Button2()
+    {
+        art2.SetActive(true);
+        playerArt.SetActive(false);
+    }
+  
+}
