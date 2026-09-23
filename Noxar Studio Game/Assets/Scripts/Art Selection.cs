7c00f018add1e1baa0e511348740f98fc22dba55
Lays-Dev <penn2ink@gmail.com>
2026-09-23 08:06:41 -0400

    page flip working savepoint

--- Noxar Studio Game/Assets/Scripts/Art Selection.cs
+++ Noxar Studio Game/Assets/Scripts/Art Selection.cs
@@ -7,6 +7,7 @@ public class ArtSelection : MonoBehaviour
 
     [SerializeField] private GameObject playerArt;
     [SerializeField] private GameObject art2;
+    [SerializeField] private float pageFlipDuration = 0.5f;
     public Button myButton;
     public Button button2;
 
@@ -21,8 +22,7 @@ public class ArtSelection : MonoBehaviour
  
     async void ButtonClick()
     {
-        await Awaitable.NextFrameAsync();
-        await Awaitable.NextFrameAsync();
+        await Awaitable.WaitForSecondsAsync(pageFlipDuration);
         playerArt.SetActive(true);
         art2.SetActive(false);
     }
